4ff7ce859d1da6fd474636fb87fd7b2710ca858c
GT <tesla.grzes@gmail.com>
2023-12-21 20:07:26 +0100

    Dodano bs

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,19 +4,28 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    int n = mat->r;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    if (mat->r != mat->c || mat->r != b->r || b->c != 1 || x->r != n || x->c != 1) {
+        fprintf(stderr, "Błąd: Niewłaściwy rozmiar macieży.\n");
+        return 2;
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        if (mat->data[i][i] == 0) {
+            fprintf(stderr, "Błąd: Wystąpiło dzielenie przez zero\n");
+            return 1;
+        }
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+        x->data[i][0] = b->data[i][0] / mat->data[i][i];
 
-				return 0;
+        for (int j = i + 1; j < n; j++) {
+            x->data[i][0] -= (mat->data[i][j] * x->data[j][0]) / mat->data[i][i];
+        }
+    }
+
+    return 0;
 }
+
 
 
